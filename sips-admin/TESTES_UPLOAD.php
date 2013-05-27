720da792491055783d2479b99bad8cb8189e28c2
Ricardo Teixeira <rteixeira@finesource.pt>
2013-05-27 19:16:25 +0100

    clean

--- sips-admin/TESTES_UPLOAD.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates  asdasd  dsfdsf  df df 
- * and open the template in the editor.
- */
-?>
--- sips-admin/sms/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../dbconnect.php';
-if(0 OR 1)
-{
-echo date("i");
-}
-?>
