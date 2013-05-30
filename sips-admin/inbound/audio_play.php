e14875a77d0bd232e2a8d883d171103a5b6f2fa4
Ricardo Teixeira <rteixeira@finesource.pt>
2013-05-30 14:01:34 +0100

    Correcção no nos system commands, acrescentei o escapecmdarg e uma função para trocar caracteres especiais em normais

--- sips-admin/inbound/audio_play.php
+++ sips-admin/inbound/audio_play.php
@@ -11,7 +11,7 @@ $file=$_POST["file"];
 $original=$ast_sounds_path.$file;
 $new=  preg_replace('"\.(gsm|wav|sln)$"', ".ogg", $apache_path."sips-admin/inbound/tmp/".basename($file));
 
- $soxCommand="sox '".escapeshellcmd($original)."' '".escapeshellcmd($new)."' pad 0 0.25";
+ $soxCommand="sox ".escapeshellarg($original)." ".escapeshellarg($new)." pad 0 0.25";
  exec($soxCommand, $output, $result);
 
             //  Deal with result
--- sips-admin/inbound/upload.php
+++ sips-admin/inbound/upload.php
@@ -18,6 +18,20 @@ function log_admin($topic, $event, $id, $query, $comments = "") {
     }
 }
 
+function changespecialchars($string) {
+    $string = strtr($string, array(
+        'Á' => 'A', 'À' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Å' => 'A', 'Ä' => 'A', 'Æ' => 'AE', 'Ç' => 'C',
+        'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Í' => 'I', 'Ì' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ð' => 'Eth',
+        'Ñ' => 'N', 'Ó' => 'O', 'Ò' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O',
+        'Ú' => 'U', 'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y',
+        'á' => 'a', 'à' => 'a', 'â' => 'a', 'ã' => 'a', 'å' => 'a', 'ä' => 'a', 'æ' => 'ae', 'ç' => 'c',
+        'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e', 'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'eth',
+        'ñ' => 'n', 'ó' => 'o', 'ò' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o',
+        'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ü' => 'u', 'ý' => 'y',
+        'ß' => 'sz', 'þ' => 'thorn', 'ÿ' => 'y','\''=>''));
+    return $string;
+}
+
 $maxFileSize = 10 * pow(1024, 2); #10mb
 $ast_sounds_path = "/var/lib/asterisk/sounds/";
 if ($_FILES["audio-file"]["error"] > 0) {
@@ -43,16 +57,16 @@ if ($_FILES["audio-file"]["error"] > 0) {
             die();
             
         } else {
-            
-            move_uploaded_file($_FILES["audio-file"]["tmp_name"], $ast_sounds_path . "upload/" . $_FILES["audio-file"]["name"]);
-            $original = $ast_sounds_path . "upload/" . $_FILES["audio-file"]["name"];
-            $new = preg_replace('"\.(ogg|wav|mp3)$"', ".gsm", $ast_sounds_path . "upload/" . $_FILES["audio-file"]["name"]);
+            $filename=  changespecialchars($_FILES["audio-file"]["name"]);
+            move_uploaded_file($_FILES["audio-file"]["tmp_name"], $ast_sounds_path . "upload/" .$filename);
+            $original = $ast_sounds_path . "upload/" . $filename;
+            $new = preg_replace('"\.(ogg|wav|mp3)$"', ".gsm", $ast_sounds_path . "upload/" . $filename);
 
-            $soxCommand = "sox '$original' -r 8000 -c1 '$new' lowpass 4000 compand 0.02,0.05 -60,-60,-30,-10,-20,-8,-5,-8,-2,-8 -8 -7 0.05 resample -ql";
+            $soxCommand = "sox ".escapeshellarg($original)." -r 8000 -c1 ".escapeshellarg($new)." lowpass 4000 compand 0.02,0.05 -60,-60,-30,-10,-20,-8,-5,-8,-2,-8 -8 -7 0.05 resample -ql";
 
             //  run SOX command
             exec($soxCommand, $output, $result);
-            unlink($ast_sounds_path . "upload/" . $_FILES["audio-file"]["name"]);
+            unlink($ast_sounds_path . "upload/" . $filename);
             //  Deal with result
             if ($result != 0) {
                 header('HTTP/1.1 500 Internal Server Error');
@@ -68,7 +82,7 @@ if ($_FILES["audio-file"]["error"] > 0) {
             }
             
             log_admin("UPLOAD", "Sucess: File moved and converted to gsm", "", "");
-            echo json_encode(array("path" => "upload/" . preg_replace('"\.(ogg|wav|mp3|gsm|sln)$"', ".gsm",$_FILES["audio-file"]["name"])));
+            echo json_encode(array("path" => "upload/" . preg_replace('"\.(ogg|wav|mp3|gsm|sln)$"', ".gsm",$filename)));
             
         }
     }
