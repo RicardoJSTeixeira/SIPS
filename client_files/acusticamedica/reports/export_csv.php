4c1fd6a73a6568c857104bb0f0d1805b92148bab
Ricardo Teixeira <rteixeira@finesource.pt>
2013-07-02 17:20:54 +0100

    correcção de vars

--- client_files/acusticamedica/reports/export_csv.php
+++ client_files/acusticamedica/reports/export_csv.php
@@ -1178,7 +1178,7 @@ $i = 0;
                 $total_leads[$i] += $row[1];
             }
             if ($row[0] == 'NA' || $row[0] == 'NAT') {
-                $na2[$i] = $row[1];
+                $na2[$i] += $row[1];
                 $total_leads[$i] += $row[1];
             }
             /*if () {
@@ -1287,7 +1287,7 @@ $i = 0;
         fputcsv($output, array(" "), ";");
         fputcsv($output, columnmakerwithtotal(array(" ", "Total Dificuldades: "), $dificuldades), ";");
         fputcsv($output, columnmakerwithtotal(array(" ", "  FAX : "), $status_count['FAX'], false, false, $i), ";");
-        fputcsv($output, columnmakerwithtotal(array(" ", "  Não Atendeu : "), $status_count['NA'], false, false, $i), ";");
+        fputcsv($output, columnmakerwithtotal(array(" ", "  Não Atendeu : "), $na2, false, false, $i), ";");
         fputcsv($output, columnmakerwithtotal(array(" ", "  Número Inválido : "), joinarray($status_count['NAOEX'] , $status_count['I']), false, false, $i), ";");
         fputcsv($output, columnmakerwithtotal(array(" ", "  Número não pertence : "), $status_count['NNP'], false, false, $i), ";");
         fputcsv($output, columnmakerwithtotal(array(" ", "  Portabilidade : "), $status_count['P'], false, false, $i), ";");
