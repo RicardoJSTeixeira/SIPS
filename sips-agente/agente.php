d0b59b2a6a0e98210092b096cdb74e405a56e8cf
RicardoJSTeixeira <rteixeira@finesource.eu>
2013-07-24 15:26:56 +0100

    actualizaçao de post nativo para jquery

--- sips-agente/agente.php
+++ sips-agente/agente.php
@@ -442,45 +442,19 @@ if ($LogiNAJAX > 0)
 	// ################################################################################
 	// Send Request for allowable campaigns to populate the campaigns pull-down
 		function login_allowable_campaigns() 
-			{
-			var xmlhttp=false;
-			/*@cc_on @*/
-			/*@if (@_jscript_version >= 5)
-			// JScript gives us Conditional compilation, we can cope with old IE versions.
-			// and security blocked creation of the objects.
-			 try {
-			  xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-			 } catch (e) {
-			  try {
-			   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			  } catch (E) {
-			   xmlhttp = false;
-			  }
-			 }
-			@end @*/
-			if (!xmlhttp && typeof XMLHttpRequest!='undefined')
-				{
-				xmlhttp = new XMLHttpRequest();
-				}
-			if (xmlhttp) 
-				{ 
-				logincampaign_query = "&user=" + document.vicidial_form.VD_login.value + "&pass=" + document.vicidial_form.VD_pass.value + "&ACTION=LogiNCamPaigns&format=html";
-				xmlhttp.open('POST', 'vdc_db_query.php'); 
-				xmlhttp.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8');
-				xmlhttp.send(logincampaign_query); 
-				xmlhttp.onreadystatechange = function() 
-					{ 
-					if (xmlhttp.readyState == 4 && xmlhttp.status == 200) 
-						{
-						Nactiveext = null;
-						Nactiveext = xmlhttp.responseText;
-						
-						document.getElementById("LogiNCamPaigns").innerHTML = Nactiveext;
-						}
-					}
-				delete xmlhttp;
-				}
-			}
+            			{
+                                    logincampaign_query = {user:document.vicidial_form.VD_login.value, pass : document.vicidial_form.VD_pass.value ,ACTION:"LogiNCamPaigns",format:"html"};
+                                        
+                                        $.post('vdc_db_query.php',logincampaign_query,
+                                        function(data){
+                                                        Nactiveext = null;
+            						Nactiveext = data;
+            						
+            						document.getElementById("LogiNCamPaigns").innerHTML = Nactiveext;
+            						});
+                
+            			
+            			}
 	// -->
 	</script>
 
