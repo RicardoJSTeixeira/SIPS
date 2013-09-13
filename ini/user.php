fc1b0b568c12aaa0cedd506493400d3ea1217dcc
rteixeira <rteixeira@localhost.localdomain>
2013-09-13 19:32:22 +0100

    adicionado allowed_campaigns e user_level (only vars)

--- ini/user.php
+++ ini/user.php
@@ -4,6 +4,9 @@ class user {
 
     public $id;
     public $user_group;
+    public $user_level;
+    public $allowed_campaigns;
+    public $allowed_campaigns_raw;
 
     public function __construct() {
         $this->id = (isset($_SERVER['PHP_AUTH_USER'])) ? $_SERVER['PHP_AUTH_USER'] : FALSE;
@@ -11,9 +14,13 @@ class user {
         global $link;
         $this->user_group = FALSE;
         if ($this->id) {
-            $query = mysql_query("Select user_group from vicidial_users Where user='$this->id'");
+            $query = mysql_query("Select a.user_group,user_level,allowed_campaigns from vicidial_users a left join vicidial_user_groups b on a.user_group=b.user_group Where user='$this->id'") or die(mysql_error());
             while ($row = mysql_fetch_assoc($query)) {
                 $this->user_group = $row["user_group"];
+                $this->user_level = $row["user_level"];
+                $this->allowed_campaigns_raw = $row["allowed_campaigns"];
+                $this->allowed_campaigns = explode(" ",trim(rtrim($this->allowed_campaigns_raw, " -")));
+                
             }
         }
     }
