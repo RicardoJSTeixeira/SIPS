992096fbc8fc5a5d57848ad9fc076f3d404c0def
Andre Duarte <aduarte@finesource.pt>
2013-10-07 19:02:02 +0100

    Sistema de emails working sll installed to install:
    go to server atraves de ssh
    yast install php5-openssl

--- client_files/viragem/reclamacao.js
+++ client_files/viragem/reclamacao.js
@@ -2,9 +2,27 @@
 
 
 
+var tables = {por_abrir: {}, abertos: {}, fechados: {}, expirados: {}};
+var concess_mail = [];
+
+
+
 
 $(function()
 {
+      $("#form_enviar_mail").validationEngine();
+      $(".chzn-select").chosen({no_results_text: "Sem resultados"});
+
+      $.getJSON("emails.json", function(data) {
+            concess_mail = data.concessionarios;
+            var temp = "<option value=''>Selecione um Concessionário</option> ";
+            $.each(data.concessionarios, function(index) {
+
+                  temp += "<option value=" + index + ">" + this.nome + "</option>";
+            });
+            $("#concessionarios").append(temp);
+            $("#concessionarios").val("").trigger("liszt:updated");
+      });
 
 
       $("#tabs").tabs();
@@ -13,9 +31,6 @@ $(function()
       }).bind("cut copy paste", function(e) {
             e.preventDefault();
       });
-      $("#dateform").validationEngine();
-
-
 
       var modal_pa =
               {
@@ -29,7 +44,6 @@ $(function()
                     r_enviar: $("#modal_por_abrir #radio1"),
                     r_fechar: $("#modal_por_abrir #radio2")
               };
-
       var modal_af =
               {
                     nome: $("#modal_abertos_fechados #nome_cliente_af"),
@@ -39,31 +53,72 @@ $(function()
                     estado: $("#modal_abertos_fechados #checkbox1"),
                     t_reclamacao: $("#modal_abertos_fechados #tipo_reclamacao_af"),
                     tp_reclamacao: $("#modal_abertos_fechados #tipificacao_reclamacao_af"),
+                    email_destino:$("#modal_abertos_fechados #emails_destino_af")
               };
-
-
-      $("#button_send_mail").on("click", function() {
-            var info = $(this).data("info");
-            var comentario = modal_pa.textarea_c_valor.val();
-            var email = "";
-            var t_reclamacao = modal_pa.t_reclamacao.find("option:selected").text();
-            var tp_reclamacao = modal_pa.tp_reclamacao.find("option:selected").text();
-            $.post("requests.php", {action: "save_mail", lead_id: info.lead_id, nome: info.nome, campanha: info.campanha, comentario: comentario, email: email, data: info.data, tipo: $("#radio2").is(":checked"), tipo_reclamacao: t_reclamacao, tipificacao_reclamacao: tp_reclamacao}, function(data) {
-                  $("#modal_por_abrir").modal('hide');
-            }, "json")
-                    .fail(function() {
-                  return false;
-            });
+      var modal_e =
+              {
+                    nome: $("#modal_expirados #nome_cliente_e"),
+                    campanha: $("#modal_expirados #campanha_e"),
+                    data: $("#modal_expirados #data_e"),
+                    comentario: $("#modal_expirados #comentario_e"),
+                    estado: $("#modal_expirados #estado_e"),
+                    t_reclamacao: $("#modal_expirados #tipo_reclamacao_e"),
+                    tp_reclamacao: $("#modal_expirados #tipificacao_reclamacao_e"),
+                    email:$("#modal_expirados #emails_destino_e")
+              };
+      $("#button_send_mail").on("click", function()
+      {
+            if ($("#radio2").is(":checked") || $("#email_agentes").val() !== null)
+            {
+                  $("#modal_por_abrir .modal-footer button").prop("disabled", true);
+                  $("#button_send_mail").text("A Gravar...");
+                  var info = $(this).data("info");
+                  var comentario = modal_pa.textarea_c_valor.val();
+                  var t_reclamacao = modal_pa.t_reclamacao.find("option:selected").text();
+                  var tp_reclamacao = modal_pa.tp_reclamacao.find("option:selected").text();
+                  $.post("requests.php",
+                          {action: "send_mail", lead_id: info.lead_id, nome: info.nome, campanha: info.campanha, comentario: comentario, email: $("#email_agentes").val(),  tipo: $("#radio2").is(":checked"), tipo_reclamacao: t_reclamacao, tipificacao_reclamacao: tp_reclamacao},
+                  function()
+                  {
+                        $("#modal_por_abrir .modal-footer button").prop("disabled", false);
+                        $("#button_pesquisa").click();
+                        $("#modal_por_abrir").modal("hide");
+                        $("#button_send_mail").text("Gravar");
+                  }, "json")
+                          .fail(function() {
+                        $("#modal_por_abrir .modal-footer button").prop("disabled", false);
+                        $("#button_pesquisa").click();
+                        $("#modal_por_abrir").modal("hide");
+                        $("#button_send_mail").text("Gravar");
+                        $.jGrowl('O Email não foi enviado', {life: 3000});
+                        return false;
+                  });
+            }
+            else
+            {
+                  $.jGrowl('Escolha um ou vários emails', {life: 3000});
+            }
       });
+      $("#button_gravar").on("click", function()
+      {
+            $("#button_pesquisa").click();
+            if ($("#modal_abertos_fechados #checkbox1").is(":checked"))
+            {
+                  $.post("requests.php", {action: "edit_estado", id: $(this).data("id")}, function(data) {
 
-
-
-
-
+                  }, "json")
+                          .fail(function() {
+                        return false;
+                  });
+            }
+      });
       $(document).on("click", ".ver_reclamacao", function()
       {
+            $("#email_agentes").empty().trigger("liszt:updated");
+            $("#concessionarios").val("").trigger("liszt:updated");
             var info = $(this).data("info");
-            switch ($(this).data("tipo"))
+            $("#modal_abertos_fechados #button_gravar").prop("disabled", false);
+            switch (info.tipo)
             {
                   case ("por_abrir"):
                         modal_pa.nome.text(info.nome);
@@ -84,96 +139,125 @@ $(function()
                         });
                         $("#modal_por_abrir").modal('show');
                         break;
-
                   case ("abertos"):
                         modal_af.nome.text(info.nome);
                         modal_af.campanha.text(info.campanha);
                         modal_af.data.text(info.data);
+                        modal_af.comentario.text(info.comentario);
+                        modal_af.t_reclamacao.text(info.tipo_reclamacao);
+                        modal_af.tp_reclamacao.text(info.tipificacao_reclamacao);
+                        modal_af.email_destino.text(info.email);
+                        $("#modal_abertos_fechados #myModalLabel").text("Reclamações abertas");
                         $("#checkbox1").prop("checked", false);
+                        $("#modal_abertos_fechados #button_gravar").data("id", this.id.slice(0, -1));
                         $("#modal_abertos_fechados").modal('show');
                         break;
-
                   case ("fechados"):
+
+                        modal_af.nome.text(info.nome);
+                        modal_af.campanha.text(info.campanha);
+                        modal_af.data.text(info.data);
+                        modal_af.comentario.text(info.comentario);
+                        modal_af.t_reclamacao.text(info.tipo_reclamacao);
+                        modal_af.tp_reclamacao.text(info.tipificacao_reclamacao);
+                         modal_af.email_destino.text(info.email);
+                        $("#modal_abertos_fechados #myModalLabel").text("Reclamações fechadas");
                         $("#checkbox1").prop("checked", true);
+                        $("#modal_abertos_fechados #button_gravar").prop("disabled", true);
                         $("#modal_abertos_fechados").modal('show');
                         break;
-
-                  case ("expirados"):
+                  default:
+                        modal_e.nome.text(info.nome);
+                        modal_e.campanha.text(info.campanha);
+                        modal_e.data.text(info.data);
+                        modal_e.comentario.text(info.comentario);
+                        modal_e.estado.text(info.tipo);
+                        modal_e.t_reclamacao.text(info.tipo_reclamacao);
+                        modal_e.tp_reclamacao.text(info.tipificacao_reclamacao);
+                         modal_e.email_destino.text(info.email);
+                        modal_e.data.text(info.data);
                         $("#modal_expirados").modal('show');
                         break;
             }
 
 
       });
-
       $(document).on("change", "#select_comentario", function()
       {
             modal_pa.textarea_c_valor.text(modal_pa.select_c.find("option:selected").data("valor"));
       });
+//TABLE POR ABRIR
+      tables.por_abrir = $('#table_por_abrir').dataTable({
+            "bProcessing": true,
+            "aoColumns": [{"bSortable": true}, {"bSortable": true}, {"bSortable": true}]
+      });
+      // TABLE ABERTOS
+      tables.abertos = $('#table_abertos').dataTable({
+            "bProcessing": true,
+            "aoColumns": [{"bSortable": true}, {"bSortable": true}, {"bSortable": true}, {"bSortable": true}, {"bSortable": true}]
+      });
+      //TABLE FECHADOS
+      tables.fechados = $('#table_fechados').dataTable({
+            "bProcessing": true,
+            "aoColumns": [{"bSortable": true}, {"bSortable": true}, {"bSortable": true}, {"bSortable": true}, {"bSortable": true}]
+      });
+      //TABLE EXPIRADOS
+      tables.expirados = $('#table_expirados').dataTable({
+            "bProcessing": true,
+            "aoColumns": [{"bSortable": true}, {"bSortable": true}, {"bSortable": true}, {"bSortable": true}, {"bSortable": true}, {"bSortable": true}]
+      });
 });
+
+
+$("#concessionarios").on("change", function()
+{
+      $("#email_agentes").empty();
+      var temp = "";
+      if ($(this, "option:selected").val() >= "0")
+            $.each(concess_mail[$(this, "option:selected").val()].interlocutores, function()
+            {
+                  temp += "<option value=" + this.email + ">" + this.nome + "</option>";
+            });
+      $("#email_agentes").append(temp);
+      $("#email_agentes option").prop("selected",true);
+      $("#email_agentes").trigger("liszt:updated");
+});
+
 
 
 $("#button_pesquisa").on("click", function()
 {
-      $("#report_reclamacao").removeClass("hidden");
       if ($("#dateform").validationEngine('validate'))
       {
-            $(".table_tbody").empty();
-            var table_body = "";
-            $.post("requests.php", {action: "get_table_data_new", data_inicio: $("#datetime_from").val(), data_fim: $("#datetime_to").val()}, function(data) {
-                  $.each(data, function() {
-                        $("#table_por_abrir .table_tbody").append($("<tr>")
-                                .append($("<td>").text(this.nome))
-                                .append($("<td>").text(this.campanha))
-                                .append($("<td>").text(this.data)
-                                .append($("<div>").addClass("view-button").append($("<button>").addClass("btn icon-reorder ver_reclamacao").data("tipo", "por_abrir").data("info", this).text(" Ver"))))
-                                );
+            $("#report_reclamacao").removeClass("hidden");
+            $.post("requests.php", {action: "get_table_data", data_inicio: $("#datetime_from").val(), data_fim: $("#datetime_to").val()}, function(data) {
+                  tables.por_abrir.fnClearTable();
+                  $.each(data.por_abrir, function() {
+                        tables.por_abrir.dataTable().fnAddData(
+                                [this.nome, this.campanha, this.data + "<div class='view-button'><button id='" + this.lead_id + "L' class='btn btn-mini icon-reorder ver_reclamacao'> Ver </button></div"]);
+                        $("#" + this.lead_id + "L").data("info", this);
                   });
-                  $.post("requests.php", {action: "get_table_data_resolved", data_inicio: $("#datetime_from").val(), data_fim: $("#datetime_to").val()}, function(data) {
-                        $.each(data, function() {
-
-                              if (this.tipo)
-                                    table_body = $("#table_fechados .table_tbody");
-                              else
-                                    table_body = $("#table_abertos .table_tbody");
-
-                              table_body.append($("<tr>")
-                                      .append($("<td>").text(this.nome))
-                                      .append($("<td>").text(this.campanha))
-                                      .append($("<td>").text(this.tipo_reclamacao))
-                                      .append($("<td>").text(this.tipificacao_reclamacao))
-                                      .append($("<td>").text(this.data)
-                                      .append($("<div>").addClass("view-button").append($("<button>").addClass("btn icon-reorder ver_reclamacao").data("tipo", (this.tipo) ? "abertos" : "fechados").data("info", this).text(" Ver"))))
-                                      );
-                        });
-                        $.post("requests.php", {action: "get_table_data_expired"}, function(data) {
-                              table_body = $("#table_expirados .table_tbody");
-                              $.each(data, function() {
-                                    table_body.append($("<tr>")
-                                            .append($("<td>").text(this.nome))
-                                            .append($("<td>").text(this.campanha))
-                                            .append($("<td>").text(this.tipo_reclamacao))
-                                            .append($("<td>").text(this.tipificacao_reclamacao))
-                                            .append($("<td>").text(this.data))
-                                            .append($("<td>").text((this.tipo) ? "aberto" : "fechado")
-                                            .append($("<div>").addClass("view-button").append($("<button>").addClass("btn icon-reorder  ver_reclamacao").data("tipo", "expirado").data("info", this).text(" Ver"))))
-                                            );
-                              });
-                        }, "json")
-                                .fail(function() {
-                              return false;
-                        });
-                  }, "json")
-                          .fail(function() {
-                        return false;
+                  tables.abertos.fnClearTable();
+                  $.each(data.abertos, function() {
+                        tables.abertos.dataTable().fnAddData(
+                                [this.nome, this.campanha, this.tipo_reclamacao, this.tipificacao_reclamacao, this.data + "<div class='view-button'><button id='" + this.id + "I' class='btn btn-mini icon-reorder ver_reclamacao'> Ver </button></div"]);
+                        $("#" + this.id + "I").data("info", this);
+                  });
+                  tables.fechados.fnClearTable();
+                  $.each(data.fechados, function() {
+                        tables.fechados.dataTable().fnAddData(
+                                [this.nome, this.campanha, this.tipo_reclamacao, this.tipificacao_reclamacao, this.data + "<div class='view-button'><button id='" + this.id + "I' class='btn btn-mini icon-reorder ver_reclamacao'> Ver </button></div"]);
+                        $("#" + this.id + "I").data("info", this);
+                  });
+                  tables.expirados.fnClearTable();
+                  $.each(data.expirados, function() {
+                        tables.expirados.dataTable().fnAddData(
+                                [this.nome, this.campanha, this.tipo_reclamacao, this.tipificacao_reclamacao, this.data, this.tipo + "<div class='view-button'><button id='" + this.id + "I' class='btn btn-mini icon-reorder ver_reclamacao'> Ver </button></div"]);
+                        $("#" + this.id + "I").data("info", this);
                   });
             }, "json")
                     .fail(function() {
                   return false;
             });
       }
-
-
 });
-
-
--- client_files/viragem/requests.php
+++ client_files/viragem/requests.php
@@ -2,7 +2,10 @@
 
 require("../../ini/dbconnect.php");
 require("../../ini/user.php");
+require ('../../swiftemail/lib/swift_required.php');
 
+error_reporting();
+ini_set('display_errors', '1');
 
 foreach ($_POST as $key => $value) {
     ${$key} = $value;
@@ -11,43 +14,55 @@ foreach ($_GET as $key => $value) {
     ${$key} = $value;
 }
 
-$js = array();
+$js["por_abrir"] = array();
+$js["fechados"] = array();
+$js["abertos"] = array();
+$js["expirados"] = array();
 switch ($action) {
     //------------------------------------------------//    
     //---------------------GET------------------------//  
     //------------------------------------------------//
 
-    case "get_table_data_new":
-        $query = "SELECT a.lead_id,c.first_name,a.campaign_id,b.campaign_name, a.call_date from vicidial_log a inner join vicidial_campaigns b on a.campaign_id=b.campaign_id left join vicidial_list c on a.lead_id=c.lead_id where a.status='S00014' and call_date between '$data_inicio' and '$data_fim'";
+    case "get_table_data":
+
+
+
+        //ABERTOS E FECHADOS
+        $query = "SELECT id,lead_id,nome,campanha,comentario,email,data,tipo,tipo_reclamacao,tipificacao_reclamacao from reclamacao  where data between '$data_inicio' and '$data_fim'";
         $query = mysql_query($query, $link) or die(mysql_error());
         while ($row = mysql_fetch_assoc($query)) {
-            $js[] = array("lead_id" => $row["lead_id"], "nome" => $row["first_name"], "campaign_id" => $row["campaign_id"], "campanha" => $row["campaign_name"], "data" => $row["call_date"]);
+            if ((int) $row["tipo"])
+                $js["fechados"][] = array("nome" => $row["nome"], "campanha" => $row["campanha"], "tipo_reclamacao" => $row["tipo_reclamacao"], "tipificacao_reclamacao" => $row["tipificacao_reclamacao"], "data" => $row["data"], "id" => $row["id"], "lead_id" => $row["lead_id"], "comentario" => $row["comentario"], "email" => json_decode($row["email"]), "tipo" => ((int) $row["tipo"]) ? "fechados" : "abertos");
+            else
+                $js["abertos"][] = array("nome" => $row["nome"], "campanha" => $row["campanha"], "tipo_reclamacao" => $row["tipo_reclamacao"], "tipificacao_reclamacao" => $row["tipificacao_reclamacao"], "data" => $row["data"], "id" => $row["id"], "lead_id" => $row["lead_id"], "comentario" => $row["comentario"], "email" => json_decode($row["email"]), "tipo" => ((int) $row["tipo"]) ? "fechados" : "abertos");
         }
-        echo json_encode(isset($js) ? $js : array());
-        break;
 
-    case "get_table_data_resolved":
-        $query = "SELECT id,lead_id,nome,campanha,comentario,data,tipo,tipo_reclamacao,tipificacao_reclamacao from reclamacao  where data between '$data_inicio' and '$data_fim'";
+//EXPIRADOS
+        $date = date("Y-m-d H:i:s", strtotime('-1 month'));
+        $query = "SELECT id,lead_id,nome,campanha,comentario,email,data,tipo,tipo_reclamacao,tipificacao_reclamacao from reclamacao where data<'$date' and tipo='0' order  by data desc";
         $query = mysql_query($query, $link) or die(mysql_error());
         while ($row = mysql_fetch_assoc($query)) {
-            $js[] = array("id" => $row["id"], "lead_id" => $row["lead_id"], "nome" => $row["nome"], "campanha" => $row["campanha"], "comentario" => $row["comentario"], "data" => $row["data"], "tipo" => (int) $row["tipo"], "tipo_reclamacao" => $row["tipo_reclamacao"], "tipificacao_reclamacao" => $row["tipificacao_reclamacao"]);
+            $leads = array_push($leads, $row["lead_id"]);
+            $js["expirados"][] = array("nome" => $row["nome"], "campanha" => $row["campanha"], "tipo_reclamacao" => $row["tipo_reclamacao"], "tipificacao_reclamacao" => $row["tipificacao_reclamacao"], "data" => $row["data"], "id" => $row["id"], "lead_id" => $row["lead_id"], "comentario" => $row["comentario"], "email" => json_decode($row["email"]), "tipo" => ((int) $row["tipo"]) ? "fechado" : "aberto");
         }
-        echo json_encode(isset($js) ? $js : array());
-        break;
 
-    case "get_table_data_expired":
 
-        $date = date("Y-m-d_H:i:s", strtotime('-1 month'));
-        $query = "SELECT id,lead_id,nome,campanha,comentario,data,tipo from reclamacao where data<'$date' order by data desc";
+
+        //POR ABRIR
+        $query = "SELECT a.lead_id,c.first_name,a.campaign_id,b.campaign_name, a.call_date from vicidial_log a inner join vicidial_campaigns b on a.campaign_id=b.campaign_id left join vicidial_list c on a.lead_id=c.lead_id left join reclamacao d on d.lead_id=a.lead_id where a.status='S00014' and d.lead_id is NULL and call_date between '$data_inicio' and '$data_fim'";
         $query = mysql_query($query, $link) or die(mysql_error());
         while ($row = mysql_fetch_assoc($query)) {
-            $js[] = array("id" => $row["id"], "lead_id" => $row["lead_id"], "nome" => $row["nome"], "campanha" => $row["campanha"], "comentario" => $row["comentario"], "data" => $row["data"], "tipo" => (int) $row["tipo"], "tipo_reclamacao" => $row["tipo_reclamacao"], "tipificacao_reclamacao" => $row["tipificacao_reclamacao"]);
+            $js["por_abrir"][] = array("nome" => $row["first_name"], "campanha" => $row["campaign_name"], "data" => $row["call_date"], "campaign_id" => $row["campaign_id"], "lead_id" => $row["lead_id"], "tipo" => "por_abrir");
         }
+
+
+
         echo json_encode($js);
         break;
 
     case "get_script_fields":
-        $query = "SELECT a.id,a.tag,a.type,c.valor from script_dinamico a inner join script_assoc b on a.id_script=b.id_script inner join script_result c on a.tag=c.tag_elemento and lead_id=$lead_id   where b.id_camp_linha='$campaign_id' and a.type in ('textarea','texto')";
+        $ja = array();
+        $query = "SELECT a.id,a.tag,a.type,c.valor from script_dinamico a inner join script_assoc b on a.id_script=b.id_script inner join script_result c on a.tag=c.tag_elemento and lead_id='$lead_id'  where b.id_camp_linha='$campaign_id' and a.type in ('textarea','texto')";
         $query = mysql_query($query, $link) or die(mysql_error());
         while ($row = mysql_fetch_assoc($query)) {
             switch ($row["type"]) {
@@ -57,16 +72,97 @@ switch ($action) {
                 case "texto";
                     $row["type"] = "Caixa de Texto";
             }
-            $js[] = array("id" => $row["id"], "tag" => $row["tag"], "type" => $row["type"], "valor" => $row["valor"]);
+            $ja[] = array("id" => $row["id"], "tag" => $row["tag"], "type" => $row["type"], "valor" => $row["valor"]);
         }
-        echo json_encode($js);
+        echo json_encode($ja);
         break;
 
 
-    case "save_mail":
-        $query = "INSERT INTO `reclamacao`(`id`, `lead_id`, `nome`, `campanha`, `comentario`, `email`, `data`, `tipo`,tipo_reclamacao,tipificacao_reclamacao) VALUES (NULL,$lead_id,'$nome','$campanha','$comentario','$email','$data',$tipo,'$tipo_reclamacao','$tipificacao_reclamacao')";
+
+    case "edit_estado":
+        $query = "UPDATE `reclamacao` SET tipo=1 WHERE id=$id";
         $query = mysql_query($query, $link) or die(mysql_error());
         echo json_encode(1);
         break;
+
+    case "send_mail":
+        $result = 1;
+        //save to DB
+        $query = "INSERT INTO `reclamacao`(`id`, `lead_id`, `nome`, `campanha`, `comentario`, `email`, `data`, `tipo`,tipo_reclamacao,tipificacao_reclamacao) VALUES (NULL,$lead_id,'$nome','$campanha','$comentario','" . mysql_real_escape_string(json_encode($email)) . "','".date("Y-m-d H:i:s")."',$tipo,'$tipo_reclamacao','$tipificacao_reclamacao')";
+        $query = mysql_query($query, $link) or die(mysql_error());
+
+        if ($tipo) {
+            $transport = Swift_SmtpTransport::newInstance('mail.viragem.com', 465, 'ssl')
+                    ->setUsername('viragem@viragem.com')
+                    ->setPassword('password12345##');
+
+            $mailer = Swift_Mailer::newInstance($transport);
+
+            // Create the message
+            $message = Swift_Message::newInstance();
+
+
+
+
+
+            $query = "SELECT date_of_birth,address3,address2,last_name,middle_initial from vicidial_list where lead_id='$lead_id'";
+            $query = mysql_query($query, $link) or die(mysql_error());
+            $row = mysql_fetch_assoc($query);
+
+
+            $struture =
+                    "
+Ex.mo(ma) Senhor(a), 
+ 
+ 
+Assunto da Reclamação:$tipo_reclamacao 
+ 
+Descrição da Reclamação:  $tipificacao_reclamacao 
+ 
+Data da Visita:". $row["date_of_birth"] ."
+ 
+Data da Reclamação: $date
+ 
+ 
+ 
+Dados do contacto
+
+
+Nome: " . $nome . "
+Telemóvel: ". $row["address3"] ."
+ 
+Marca: ". $row["address2"] ."
+Modelo: ". $row["last_name"] ."
+Matrícula: ". $row["middle_initial"] ."
+ 
+ 
+$comentario
+  
+ 
+Atentamente 
+             ";
+
+
+
+
+            $message
+                    ->setFrom(array('viragem@viragem.com' => 'Viragem'))
+                    ->setSubject($tipo_reclamacao)
+                    ->setBody($struture);
+            foreach ($email as $value) {
+                $message->addTo($value);
+            }
+            $result = $mailer->send($message);
+        }
+        echo json_encode($result >= 1);
+
+        break;
 }
 ?>
+
+
+
+
+
+
+
