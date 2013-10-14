87816dcb05d8f42c85bd09a020a9d7d127bf3dd8
Andre Duarte <aduarte@finesource.pt>
2013-10-14 10:58:36 +0100

    Bug fixed no upload

--- sips-admin/script_dinamico/upload.php
+++ sips-admin/script_dinamico/upload.php
@@ -9,22 +9,24 @@ $destiny = getcwd() . "/files/";
 
 
 if ($extension == "pdf") {
-              if (file_exists($destiny . $_FILES["file"]["name"])) {
-                echo $_FILES["file"]["name"] . " Já existe. ";
-            } else {
-                move_uploaded_file($_FILES["file"]["tmp_name"], $destiny . $_FILES["file"]["name"]);
-                echo "Guardado";
-            }
+    if (file_exists($destiny . $_FILES["file"]["name"])) {
+        echo $_FILES["file"]["name"] . " Já existe. ";
+    } else {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $destiny . $_FILES["file"]["name"]))
+            echo "Guardado";
+        else
+            echo "Não Guardado";
+    }
+} else if ($extension == "gif" || $extension == "jpeg" || $extension == "jpg" || $extension == "png") {
+    if (file_exists($destiny . $_FILES["file"]["name"])) {
+        echo $_FILES["file"]["name"] . " Já existe. ";
+    } else {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $destiny . $_FILES["file"]["name"]))
+            echo "Guardado";
+        else
+            echo "Não Guardado";
+    }
 }
-else if($extension == "gif"||$extension == "jpeg"||$extension == "jpg"||$extension == "png")
-{
-            if (file_exists($destiny . $_FILES["file"]["name"])) {
-                echo $_FILES["file"]["name"] . " Já existe. ";
-            } else {
-                move_uploaded_file($_FILES["file"]["tmp_name"], $destiny . $_FILES["file"]["name"]);
-                echo "Guardado";
-            }
-     }
 else
-     echo "Ficheiro Inválido";
+    echo "Ficheiro Inválido";
 ?>
