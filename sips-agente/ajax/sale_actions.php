70a5245846fc6e7f85fb2b371fdd0d82b3bbd36c
rteixeira <rteixeira@localhost.localdomain>
2013-11-08 21:29:44 +0000

    update

--- sips-agente/ajax/sale_actions.php
+++ sips-agente/ajax/sale_actions.php
@@ -47,11 +47,9 @@ function connectaPostCalendar() {
     $query = "SELECT start_date, id_resource FROM sips_sd_reservations where lead_id = '$lead_id' and start_date > DATE(NOW()) LIMIT 1";
     $query = mysql_query($query, $link) or die(mysql_error());
     $row = mysql_fetch_row($query);
-    $data = date('d/m/Y',strtotime($row[0]));
-    $hora = date('h:i',strtotime($row[0]));
     $distrito = ''; // vazio
-    $data_visita = $data; // dd/mm/yyyy -> calendario
-    $hora_visita = $hora; // hh:mm -> calendario
+    $data_visita = date('d/m/Y',strtotime($row[0]));; // dd/mm/yyyy -> calendario
+    $hora_visita =  date('h:i',strtotime($row[0]));; // hh:mm -> calendario
     $query = "SELECT a.display_text FROM sips_sd_schedulers a inner join sips_sd_resources b on a.id_scheduler = b.id_scheduler where id_resource = '$row[1]'";
     $query = mysql_query($query, $link) or die(mysql_error());
     $row = mysql_fetch_row($query);
