a9a0915c8a75163120d8484256e108a6710bd955
rteixeira <rteixeira@localhost.localdomain>
2013-11-08 21:29:58 +0000

    update

--- sips-agente/ajax/sale_actions.php
+++ sips-agente/ajax/sale_actions.php
@@ -48,8 +48,8 @@ function connectaPostCalendar() {
     $query = mysql_query($query, $link) or die(mysql_error());
     $row = mysql_fetch_row($query);
     $distrito = ''; // vazio
-    $data_visita = date('d/m/Y',strtotime($row[0]));; // dd/mm/yyyy -> calendario
-    $hora_visita =  date('h:i',strtotime($row[0]));; // hh:mm -> calendario
+    $data_visita = date('d/m/Y',strtotime($row[0])); // dd/mm/yyyy -> calendario
+    $hora_visita =  date('h:i',strtotime($row[0])); // hh:mm -> calendario
     $query = "SELECT a.display_text FROM sips_sd_schedulers a inner join sips_sd_resources b on a.id_scheduler = b.id_scheduler where id_resource = '$row[1]'";
     $query = mysql_query($query, $link) or die(mysql_error());
     $row = mysql_fetch_row($query);
