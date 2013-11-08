817106f1cbd9317d979ee4443c8627c59d89ec0d
Andre Duarte <aduarte@finesource.pt>
2013-11-08 15:21:00 +0000

    adicionado filtro de USERONLY

--- sips-agente/ajax/callbacks.php
+++ sips-agente/ajax/callbacks.php
@@ -10,7 +10,7 @@ foreach ($_GET as $key => $value) {
 }
 
 $js = array();
-$query = "SELECT count(callback_id) as total FROM `vicidial_callbacks` WHERE campaign_id='$campaign_id' and user='$user' and status <> 'INACTIVE'";
+$query = "SELECT count(callback_id) as total FROM `vicidial_callbacks` WHERE campaign_id='$campaign_id' and user='$user' and recipient='USERONLY' and status <> 'INACTIVE'";
 $query = mysql_query($query, $link) or die(mysql_error());
 $row = mysql_fetch_assoc($query);
 echo json_encode($row["total"]);
