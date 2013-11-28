83dea4d0e52ae177233f17361d44657d77b85480
AndreDuart <aduarte@finesource.pt>
2013-11-28 18:49:58 +0000

    Update script.php

--- sips-admin/script_dinamico/script.php
+++ sips-admin/script_dinamico/script.php
@@ -556,11 +556,16 @@ class script {
                 }
             }
         }
+        
+        if(count($results))
+        {
         $query = "INSERT INTO `script_result`(`date`,`id_script`,`user_id`,`unique_id`,`campaign_id`,`lead_id`, `tag_elemento`, `valor`,`param_1`) VALUES  " . implode(',', $sql);
 
         $stmt = $this->db->prepare($query);
-        return  $stmt->execute() ;
-         
+    return  $stmt->execute() ;
+        }
+        else
+            return  "no Data to be saved" ;
     }
 
 }
