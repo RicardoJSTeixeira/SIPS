d9dd2e66f6ab0c1741e68be2e4c5f9fa98544726
Andre Duarte <aduarte@finesource.pt>
2013-11-28 15:54:44 +0000

    new db file

--- ini/db.php
+++ ini/db.php
@@ -1,8 +1,7 @@
 <?php
-for ($i = 0; $i < count(explode('/', $_SERVER['PHP_SELF'])) - 2; $i++) {$far.="../";}
-define("ROOT", $far);
 
-$json_data = file_get_contents(ROOT."/AM/config/db.json");
+
+$json_data = file_get_contents("db.json");
 $db_config = (object) json_decode($json_data);
 
 $host = "mysql:host=" . $db_config->ip . ";dbname=" . $db_config->database . ";charset=utf8";
