b0a0f55b5583f36db010a01b392b5c55eb5eb41e
i13 <i13@localhost.localdomain>
2013-12-17 18:45:50 +0000

    Criação de php para total e avg dos graficos.

---
+++ ivrtts/intra_realtime/avg.php
@@ -0,0 +1,45 @@
+<?php
+
+$tempo = $_POST['temporal'];
+$id = $_POST['id'];
+
+$file = file_get_contents("http://localhost:10000/ccstats/v0/avg/calls/length_in_sec?by=database.campaign,status,$tempo&database.campaign.oid=$id");
+
+$content = json_decode($file, true);
+
+$p = array('msg' => array(), 'sys' => array(), 'total' => array());
+foreach ($content as $value) {
+
+    $p['total'][$value['_id'][$tempo]]+=$value['avg'];
+
+    switch (this . _id . status . oid) {
+        case "MSG001":
+        case "MSG002":
+        case "MSG003":
+        case "MSG004":
+        case "MSG005":
+        case "MSG006":
+        case "MSG007":
+        case "NEW":
+            $p['msg'][$value['_id'][$tempo]]+=$value['avg'];
+            break;
+        default :
+            $p['sys'][$value['_id'][$tempo]]+=$value['avg'];
+            break;
+    }
+}
+$msg=array();
+foreach ($p['msg'] as $key=>$value) {
+    $msg[]=(object)array("x"=>$key,"y"=>$value);
+}
+$sys=array();
+foreach ($p['sys'] as $key=>$value) {
+    $sys[]=(object)array("x"=>$key,"y"=>$value);
+}
+$total=array();
+foreach ($p['total'] as $key=>$value) {
+    $total[]=(object)array("x"=>$key,"y"=>$value);
+}
+    
+
+echo json_encode(array('msg'=>$msg,'sys'=>$sys,'total'=>$total));
