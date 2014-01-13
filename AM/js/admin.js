412011cd8c490d8e1052683a11193023b8830b6b
andre <andre@andre-workstation>
2014-01-13 19:11:21 +0000

    further development no spice (parte de admin)

--- AM/js/admin.js
+++ AM/js/admin.js
@@ -1,65 +1,216 @@
+
+
 $(function()
 {
-    $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd', autoclose: true, language: "pt", minView: 2});
+    $("#admin_zone .form_datetime").datetimepicker({format: 'yyyy-mm-dd', autoclose: true, language: "pt", minView: 2});
+    $("#admin_zone .check_form").validationEngine();
+    get_info();
+    $("#admin_zone .chosen-select").chosen({no_results_text: "Sem resultados"});
 });
 
 
-
-
-
-// PRODUTOS
-$("#view_product_button").click(function()
+$("#admin_zone .check_form").submit(function(e)
 {
-    if ($("#view_product_div").is(":visible"))
+    e.preventDefault();
+})
+
+//OpçÔES TOGGLE
+$("#admin_zone #button_filtro_produto").click(function()
+{
+    $("#admin_zone #product_master_div").toggle("blind");
+    $(this).toggleClass("icon-chevron-down").toggleClass("icon-chevron-up");
+});
+
+$("#admin_zone #button_filtro_cliente").click(function()
+{
+    $("#admin_zone #client_master_div").toggle("blind");
+    $(this).toggleClass("icon-chevron-down").toggleClass("icon-chevron-up");
+});
+//------------
+
+
+
+
+function get_info()
+{
+    var Table_view_product = $('#admin_zone #view_product_datatable').dataTable({
+        "aaSorting": [[6, "asc"]],
+        "bSortClasses": false,
+        "bProcessing": true,
+        "bDestroy": true,
+        "bAutoWidth": false,
+        "sPaginationType": "full_numbers",
+        "sAjaxSource": 'ajax/admin.php',
+        "fnServerParams": function(aoData) {
+            aoData.push({"name": "action", "value": "listar_produtos_to_datatable"});
+        },
+        "aoColumns": [{"sTitle": "id"}, {"sTitle": "Nome"}, {"sTitle": "Parente"}, {"sTitle": "Alone"}, {"sTitle": "Max requisições mês"}, {"sTitle": "Max requisições semana"}, {"sTitle": "Categoria"}, {"sTitle": "Opções"}],
+        "oLanguage": {"sUrl": "../../../jquery/jsdatatable/language/pt-pt.txt"}
+    });
+
+
+    $.post('ajax/admin.php', {action: "listar_produtos"},
+    function(data)
     {
-        $("#view_product_div").hide("blind");
-    }
-    else
-    {
-        $("#view_product_div").show("blind");
-            var Table_view_product = $('#view_product_datatable').dataTable({
+        $("#admin_zone #ep_parent").empty();
+        $("#admin_zone #ep_parent").append("<option value='0'>Escolha um parente</option>");
+        $("#admin_zone #cp_parent").empty();
+        $("#admin_zone #cp_parent").append("<option value='0'>Escolha um parente</option>");
+        var temp = "<optgroup value='1' label='Aparelhos'></optgroup>\n\
+<optgroup value='2' label='Pilhas'></optgroup>\n\
+<optgroup value='3' label='Peças'></optgroup>";
+        $("#admin_zone #ep_parent").append(temp);
+        $("#admin_zone #cp_parent").append(temp);
+        $.each(data, function()
+        {
+            switch (this.category)
+            {
+                case "aparelho":
+                    $("#admin_zone #ep_parent optgroup[value='1']").append("<option value='" + this.id + "'>" + this.name + "</option>");
+                    $("#admin_zone #cp_parent optgroup[value='1']").append("<option value='" + this.id + "'>" + this.name + "</option>");
+                    break;
+                case "pilha":
+                    $("#admin_zone #ep_parent optgroup[value='2']").append("<option value='" + this.id + "'>" + this.name + "</option>");
+                    $("#admin_zone #cp_parent  optgroup[value='2']").append("<option value='" + this.id + "'>" + this.name + "</option>");
+                    break;
+                case "peça":
+                    $("#admin_zone #ep_parent optgroup[value='3']").append("<option value='" + this.id + "'>" + this.name + "</option>");
+                    $("#admin_zone #cp_parent optgroup[value='3']").append("<option value='" + this.id + "'>" + this.name + "</option>");
+                    break;
+            }
+        });
+
+        $("#admin_zone #ep_parent").trigger("chosen:updated");
+        $("#admin_zone #cp_parent").trigger("chosen:updated");
+    }, "json");
+}
+
+//EDITAR PRODUTO
+
+$("#admin_zone").on("click", ".item_edit_button", function()
+{
+    var item_id = $(this).data("product_id")
+    $("#admin_zone #edit_product_modal").data("product_id", item_id);
+    $("#admin_zone #ep_name").val();
+
+
+    var Table_child_product = $('#admin_zone #child_product_datatable').dataTable({
         "aaSorting": [[0, "desc"]],
         "bSortClasses": false,
         "bProcessing": true,
         "bDestroy": true,
+        "bAutoWidth": false,
         "sPaginationType": "full_numbers",
         "sAjaxSource": 'ajax/admin.php',
         "fnServerParams": function(aoData) {
-            aoData.push({"name": "action", "value": "listar_produtos"});
+            aoData.push({"name": "action", "value": "listar_produtos_to_datatable_by_parent"},
+            {"name": "parent", "value": item_id});
         },
-        "aoColumns": [{"sTitle": "id"}, {"sTitle": "Nome"}, {"sTitle": "Parent"}, {"sTitle": "Alone"}, {"sTitle": "Max requisições mês"}, {"sTitle": "Max requisições semana"}, {"sTitle": "Categoria"}],
+        "aoColumns": [{"sTitle": "id"}, {"sTitle": "Nome"}, {"sTitle": "Alone"}, {"sTitle": "Max requisições mês"}, {"sTitle": "Max requisições semana"}, {"sTitle": "Categoria"}],
         "oLanguage": {"sUrl": "../../../jquery/jsdatatable/language/pt-pt.txt"}
     });
+
+
+
+    $.post('ajax/admin.php', {action: "listar_produto", id: $(this).data("product_id")},
+    function(data)
+    {
+        $("#admin_zone #ep_name").val(data.name);
+
+        $("#admin_zone #ep_parent optgroup option[value='" + data.parent + "']").prop("selected", true).trigger("chosen:updated");
+
+        if (data.alone == "1")
+            $("#ep_alone").prop("checked", true);
+        else
+            $("#ep_alone").prop("checked", false);
+        $("#admin_zone #ep_mrm").val(data.max_req_m);
+        $("#admin_zone #ep_mrw").val(data.max_req_w);
+        $("#admin_zone #ep_category").val(data.category);
+        $("#admin_zone .chosen-container").css("width", "250px");
+        $("#admin_zone #edit_product_modal").modal("show");
+    }, "json");
+
+});
+$("#admin_zone #edit_product_button").click(function()
+{
+    if ($("#admin_zone #edit_product_form").validationEngine("validate"))
+    {
+        $.post('ajax/admin.php', {action: "editar_produto",
+            id: $("#admin_zone #edit_product_modal").data("product_id"),
+            name: $("#admin_zone #ep_name").val(),
+            parent: $("#admin_zone #ep_parent option:selected").val(),
+            alone: $("#admin_zone #ep_alone").is(":checked") ? 1 : 0,
+            max_req_m: $("#admin_zone #ep_mrm").val(),
+            max_req_w:( $("#admin_zone #ep_mrm").val()>$("#admin_zone #ep_mrw").val())?$("#admin_zone #ep_mrw").val():$("#admin_zone #ep_mrm").val(),
+            category: $("#admin_zone #ep_category").val()
+        }, function() {
+            get_info();
+            $("#admin_zone #edit_product_modal").modal("hide");
+        }, "json");
     }
-
-
-
-
-
 });
-$("#add_product_button").click(function()
+//---------------------------------------------------------------------------------------------
+
+//REMOVER PRODUTO
+
+$("#admin_zone").on("click", ".item_delete_button", function()
 {
-    $("#add_product_div").toggle("blind");
-});
-$("#remove_product_button").click(function()
-{
-    $("#remove_product_div").toggle("blind");
+    $("#admin_zone  #remove_product_modal").data("product_id", $(this).data("product_id")).modal("show");
 });
 
-$("#edit_product_button").click(function()
+$("#admin_zone #remove_product_button").click(function()
 {
-    $("#edit_product_div").toggle("blind");
+    $.post('ajax/admin.php', {action: "apagar_produto",
+        id: $("#admin_zone #remove_product_modal").data("product_id")
+
+    }, function() {
+        get_info();
+        $("#admin_zone #remove_product_modal").modal("hide");
+    }, "json");
 });
+//-------------------------------------------------------------------------------------
+// CRIAR PRODUTO
+
+$("#admin_zone #create_product_modal_button").click(function()
+{
+    $("#admin_zone #create_product_modal").modal("show");
+    $("#admin_zone .chosen-container").css("width", "250px");
+});
+
+
+$("#admin_zone #create_product_button").click(function()
+{
+
+    if ($("#admin_zone #create_product_form").validationEngine("validate"))
+    {
+        $.post('ajax/admin.php', {action: "criar_produto",
+            name: $("#admin_zone #cp_name").val(),
+            parent: $("#admin_zone #cp_parent option:selected").val(),
+            alone: $("#admin_zone #cp_alone").is(":checked"),
+            max_req_m: $("#admin_zone #cp_mrm").val(),
+            max_req_w: $("#admin_zone #cp_mrw").val(),
+            category: $("#admin_zone #cp_category").val()
+
+        }, function() {
+            get_info();
+            $("#admin_zone #create_product_modal").modal("hide");
+        }, "json");
+    }
+});
+
+///------------------------------------------
+
+
 
 // CLIENTES
 // relatorio de clientes sem marcação
-$("#download_csm_button").click(function()
+$("#admin_zone #download_csm_button").click(function()
 {
-    $("#download_csm_div").toggle("blind");
+    $("#admin_zone #download_csm_div").toggle("blind");
 });
-$("#download_excel_csm_button").click(function()
+$("#admin_zone #download_excel_csm_button").click(function()
 {
-    if ($("#date_form").validationEngine("validate"))
+    if ($("#admin_zone #date_form").validationEngine("validate"))
     {
         document.location.href = "ajax/admin.php?action=download_excel_csm";
     }
--- AM/lib/products.php
+++ AM/lib/products.php
@@ -6,24 +6,34 @@ Class products {
         $this->_db = $db;
     }
 
-    public function get_products() {
-        $stmt = $this->_db->prepare("SELECT * from spice_product");
+    public function get_products_to_datatable() {
+        $stmt = $this->_db->prepare("SELECT id,name,parent,alone,max_req_m,max_req_w,category from spice_product");
         $stmt->execute();
         $output['aaData'] = $stmt->fetchAll(PDO::FETCH_BOTH);
         return $output;
     }
 
-    public function remove_product() {
-        $stmt = $this->_db->prepare("SELECT * from spice_product");
+    public function get_products_to_datatable_by_id($parent) {
+        $stmt = $this->_db->prepare("SELECT id,name,alone,max_req_m,max_req_w,category from spice_product where parent=:parent");
+        $stmt->execute(array(":parent"=>$parent));
+        $output['aaData'] = $stmt->fetchAll(PDO::FETCH_BOTH);
+        return $output;
+    }
+
+    public function get_products() {
+        $stmt = $this->_db->prepare("SELECT id,name,parent,alone,max_req_m,max_req_w,category from spice_product");
         $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function edit_product() {
-        
+    public function remove_product($id) {
+        $stmt = $this->_db->prepare("delete from spice_product where id=:id");
+        return $stmt->execute(array(":id" => $id));
     }
 
-    public function add_product() {
-        
+    public function add_product($name, $parent, $alone, $max_req_m, $max_req_w, $category) {
+        $stmt = $this->_db->prepare("insert into spice_product ( `name`, `parent`, `alone`, `max_req_m`, `max_req_w`, `category`) values (:name,:parent,:alone,:max_req_m,:max_req_w,:category) ");
+        return $stmt->execute(array(":name" => $name, ":parent" => $parent, ":alone" => $alone, ":max_req_m" => $max_req_m, ":max_req_w" => $max_req_w, ":category" => $category));
     }
 
 }
@@ -39,18 +49,40 @@ class product extends products {
     protected $_max_req_w;
     protected $_category;
 
-    public function __construct(PDO $db, $id, $type) {
+    public function __construct(PDO $db, $id) {
         $this->_db = $db;
 
-        $stmt = $this->_db->prepare("SELECT * from aaa where id=:id");
+
+        $stmt = $this->_db->prepare("SELECT id,name,parent,alone,max_req_m,max_req_w,category from spice_product where id=:id");
         $stmt->execute(array(":id" => $id));
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            
-        }
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $this->_id = $row["id"];
+        $this->_name = $row["name"];
+        $this->_parent = $row["parent"];
+        $this->_alone = $row["alone"];
+        $this->_max_req_m = $row["max_req_m"];
+        $this->_max_req_w = $row["max_req_w"];
+        $this->_category = $row["category"];
     }
 
-    public function get_info($id) {
-        return "";
+    public function edit_product($name, $parent, $alone, $max_req_m, $max_req_w, $category) {
+        $this->_name = $name;
+        $this->_parent = $parent;
+        $this->_alone = $alone;
+        $this->_max_req_m = $max_req_m;
+        $this->_max_req_w = $max_req_w;
+        $this->_category = $category;
+        return $this->edit_product_save();
+    }
+
+    public function edit_product_save() {
+        $stmt = $this->_db->prepare("update spice_product set name=:name,parent=:parent,alone=:alone,max_req_m=:max_req_m,max_req_w=:max_req_w,category=:category where id=:id");
+        return $stmt->execute(array(":id" => $this->_id, ":name" => $this->_name, ":parent" => $this->_parent, ":alone" => $this->_alone, ":max_req_m" => $this->_max_req_m, ":max_req_w" => $this->_max_req_w, ":category" => $this->_category));
+    }
+
+    public function get_info() {
+
+        return array("id" => $this->_id, "name" => $this->_name, "parent" => $this->_parent, "alone" => $this->_alone, "max_req_m" => $this->_max_req_m, "max_req_w" => $this->_max_req_w, "category" => $this->_category);
     }
 
 }
