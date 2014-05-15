c921c98184906ecee5fc6edd492ec123f5a45c08
ricardo <ricardo@ricardo-Inspiron-5521>
2014-05-15 11:13:07 +0100

    added owner ao download de bd

--- sips-admin/campaigns/extras/csv/dbdownload.php
+++ sips-admin/campaigns/extras/csv/dbdownload.php
@@ -14,9 +14,9 @@ foreach ($_GET as $key => $value) {
 header("Content-Disposition: attachment; filename=base_de_dados_" . $download_db_id . ".csv");
 $output = fopen('php://output', 'w');
 
-$control_array = array('PHONE_NUMBER', 'TITLE', 'FIRST_NAME', 'MIDDLE_INITIAL', 'LAST_NAME', 'ADDRESS1', 'ADDRESS2', 'ADDRESS3', 'CITY', 'STATE', 'PROVINCE', 'POSTAL_CODE', 'COUNTRY_CODE', 'DATE_OF_BIRTH', 'ALT_PHONE', 'EMAIL', 'SECURITY_PHRASE', 'COMMENTS', 'extra1', 'extra2', 'extra3', 'extra4', 'extra5', 'extra6', 'extra7', 'extra8', 'extra9', 'extra10', 'extra11', 'extra12', 'extra13', 'extra14', 'extra15');
+$control_array = array('PHONE_NUMBER', 'TITLE', 'FIRST_NAME', 'MIDDLE_INITIAL', 'LAST_NAME', 'ADDRESS1', 'ADDRESS2', 'ADDRESS3', 'CITY', 'STATE', 'PROVINCE', 'POSTAL_CODE', 'COUNTRY_CODE', 'DATE_OF_BIRTH', 'ALT_PHONE', 'EMAIL', 'SECURITY_PHRASE', 'OWNER', 'COMMENTS', 'extra1', 'extra2', 'extra3', 'extra4', 'extra5', 'extra6', 'extra7', 'extra8', 'extra9', 'extra10', 'extra11', 'extra12', 'extra13', 'extra14', 'extra15');
 
-$query = "SELECT status, user, phone_number, title, first_name, middle_initial, last_name, address1, address2, address3, city, state, province, postal_code, country_code, date_of_birth, alt_phone, email, security_phrase, comments, extra1, extra2, extra3, extra4, extra5, extra6, extra7, extra8, extra9, extra10, extra11, extra12, extra13, extra14, extra15 FROM vicidial_list WHERE list_id='$download_db_id'";
+$query = "SELECT status, user, phone_number, title, first_name, middle_initial, last_name, address1, address2, address3, city, state, province, postal_code, country_code, date_of_birth, alt_phone, email, security_phrase, owner, comments, extra1, extra2, extra3, extra4, extra5, extra6, extra7, extra8, extra9, extra10, extra11, extra12, extra13, extra14, extra15 FROM vicidial_list WHERE list_id='$download_db_id'";
 $result = mysql_query($query, $link) or die(mysql_error());
 
 $query = "SELECT campaign_id FROM vicidial_lists WHERE list_id='$download_db_id'";
@@ -32,15 +32,9 @@ foreach ($control_array as $key => $value) {
     $result2 = mysql_fetch_assoc($result2);
 
     if ($result2['Display_name'] == null) {
-
-
-
-        array_push($header_array, $value);
+        $header_array[]= $value;
     } else {
-
-
-
-        array_push($header_array, $result2['Display_name']);
+        $header_array[]=$result2['Display_name'];
     }
 }
 
@@ -58,8 +52,5 @@ while ($row = mysql_fetch_assoc($result)) {
         $status = mysql_fetch_assoc($status);
     }
 
-
-
-
-    fputcsv($output, array($status['status_name'], $row['user'], $row['phone_number'], $row['title'], $row['first_name'], $row['middle_initial'], $row['last_name'], $row['address1'], $row['address2'], $row['address3'], $row['city'], $row['state'], $row['province'], $row['postal_code'], $row['country_code'], $row['date_of_birth'], $row['alt_phone'], $row['email'], $row['security_phrase'], $row['comments'], $row['extra1'], $row['extra2'], $row['extra3'], $row['extra4'], $row['extra5'], $row['extra6'], $row['extra7'], $row['extra8'], $row['extra9'], $row['extra10'], $row['extra11'], $row['extra12'], $row['extra13'], $row['extra14'], $row['extra15']), ";");
+    fputcsv($output, array($status['status_name'], $row['user'], $row['phone_number'], $row['title'], $row['first_name'], $row['middle_initial'], $row['last_name'], $row['address1'], $row['address2'], $row['address3'], $row['city'], $row['state'], $row['province'], $row['postal_code'], $row['country_code'], $row['date_of_birth'], $row['alt_phone'], $row['email'], $row['security_phrase'], $row['owner'], $row['comments'], $row['extra1'], $row['extra2'], $row['extra3'], $row['extra4'], $row['extra5'], $row['extra6'], $row['extra7'], $row['extra8'], $row['extra9'], $row['extra10'], $row['extra11'], $row['extra12'], $row['extra13'], $row['extra14'], $row['extra15']), ";");
 }
