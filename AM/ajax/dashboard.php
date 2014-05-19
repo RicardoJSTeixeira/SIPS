f60c8185aef6136262fe0c60a495add5027816ca
ricardo <ricardo@ricardo-Inspiron-5521>
2014-05-19 18:52:57 +0100

    Adicionado novos campos nas datatables

--- AM/ajax/dashboard.php
+++ AM/ajax/dashboard.php
@@ -23,24 +23,24 @@ switch ($action) {
 
     case "populate_allm"://ALL MARCAÇOES
         $u = $user->getUser();
-        $variables[":list"] = $u->list_id;
         if ($u->user_level > 5) {
-            $query = "SELECT a.lead_id, b.first_name, extra1, extra2, middle_initial, postal_code, b.address1, a.start_date, a.id_user from sips_sd_reservations a 
+            $query = "SELECT a.lead_id, b.first_name, extra1, extra2, middle_initial, postal_code, b.city, b.phone_number, b.alt_phone, b.address1, IF(d.closed=1,'Fechada',IF(a.start_date>NOW(),'Marcada','Aberta')) estado, a.start_date, a.id_user from sips_sd_reservations a 
             left join vicidial_list b on a.lead_id=b.lead_id 
             inner join vicidial_users c on b.user=c.user
-            where c.user_group=:user_group and list_id=:list group by a.lead_id limit 20000";
+            left join spice_consulta d on d.reserva_id=a.id_reservation
+            where c.user_group=:user_group group by a.lead_id limit 20000";
             $variables[":user_group"] = $u->user_group;
         } else {
-            $query = "SELECT a.lead_id, b.first_name, extra1, extra2, middle_initial, postal_code, b.address1, a.start_date from sips_sd_reservations a 
+            $query = "SELECT a.lead_id, b.first_name, extra1, extra2, middle_initial, postal_code, b.city, b.phone_number, b.alt_phone, b.address1, IF(c.closed=1,'Fechada',IF(a.start_date>NOW(),'Marcada','Aberta')) estado, a.start_date from sips_sd_reservations a 
             left join vicidial_list b on a.lead_id=b.lead_id 
-            where a.id_user=:user and list_id=:list group by a.lead_id limit 20000";
+            left join spice_consulta c on c.reserva_id=a.id_reservation
+            where a.id_user=:user group by a.lead_id limit 20000";
             $variables[":user"] = $u->username;
         }
         $stmt = $db->prepare($query);
         $stmt->execute($variables);
         while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-            $row[7] = date("d-m-Y H:i:s", strtotime($row[7]));
-            $row[7] = $row[7] . "<div class='view-button'>"
+            $row[11] = $row[11] . "<div class='view-button'>"
                     . "<button class='btn btn-mini icon-alone ver_cliente' data-lead_id='$row[0]' title='Ver Cliente'><i class='icon-edit'></i></button>"
                     . "<button class='btn btn-mini icon-alone criar_encomenda' data-lead_id='$row[0]' title='Nova Encomenda'><i class='icon-shopping-cart'></i></button>"
                     . "<button class='btn btn-mini icon-alone criar_marcacao' data-lead_id='$row[0]' title='Marcar Consulta'><i class='icon-calendar'></i></button>"
@@ -56,13 +56,13 @@ switch ($action) {
         $u = $user->getUser();
         $variables[":list"] = $u->list_id;
         if ($u->user_level > 5) {
-            $query = "SELECT a.lead_id, first_name, extra1, extra2, middle_initial, postal_code, address1, a.entry_date, a.user from  vicidial_list a
+            $query = "SELECT a.lead_id, first_name, extra1, extra2, middle_initial, postal_code, city, phone_number, alt_phone, address1, a.entry_date, a.user from  vicidial_list a
                 inner join vicidial_users b on a.user=b.user
                 left join sips_sd_reservations c on a.lead_id=c.lead_id
                 where b.user_group=:user_group and list_id=:list and c.lead_id is null and extra6='NO' limit 20000";
             $variables[":user_group"] = $u->user_group;
         } else {
-            $query = "SELECT a.lead_id, first_name, extra1, extra2, middle_initial, postal_code, address1, a.entry_date from  vicidial_list a
+            $query = "SELECT a.lead_id, first_name, extra1, extra2, middle_initial, postal_code, city, phone_number, alt_phone, address1, a.entry_date from  vicidial_list a
                 left join sips_sd_reservations b on a.lead_id=b.lead_id
                 where user=:user and list_id=:list and b.lead_id is null and extra6='NO' limit 20000";
             $variables[":user"] = $u->username;
@@ -71,8 +71,7 @@ switch ($action) {
         $stmt = $db->prepare($query);
         $stmt->execute($variables);
         while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-            $row[7]= date("d-m-Y H:i:s", strtotime($row[7]));
-            $row[7] = $row[7] . "<div class='view-button'>"
+            $row[10] = $row[10] . "<div class='view-button'>"
                     . "<button class='btn btn-mini icon-alone ver_cliente' data-lead_id='$row[0]' title='Ver Cliente'><i class='icon-edit'></i></button>"
                     . "<button class='btn btn-mini icon-alone criar_marcacao' data-lead_id='$row[0]' title='Marcar Consulta'><i class='icon-calendar'></i></button>"
                     . "<button class='btn btn-mini icon-alone recomendacoes' data-lead_id='$row[0]' title='Recomendados'><i class='icon-plus-sign'></i></button>"
@@ -86,7 +85,7 @@ switch ($action) {
         $u = $user->getUser();
         $variables[":list"] = $u->list_id;
         if ($u->user_level > 5) {
-            $query = "SELECT b.first_name, b.extra2, a.lead_id, a.first_name, a.extra1, a.extra2, a.middle_initial, a.postal_code, a.address1, a.entry_date, a.user 
+            $query = "SELECT b.first_name, b.extra2, a.lead_id, a.first_name, a.extra1, a.extra2, a.middle_initial, a.postal_code, a.city, a.phone_number, a.alt_phone, a.address1, a.entry_date, a.user 
                 FROM `vicidial_list` a 
                 inner join `vicidial_list` b on a.extra7=b.lead_id 
                 inner join vicidial_users c on a.user=c.user
@@ -94,7 +93,7 @@ switch ($action) {
                 where c.user_group=:user_group and a.list_id=:list and d.lead_id is null and a.extra6='NO' limit 20000";
             $variables[":user_group"] = $u->user_group;
         } else {
-            $query = "SELECT b.first_name, b.extra2, a.lead_id, a.first_name, a.extra1, a.extra2, a.middle_initial, a.postal_code, a.address1, a.entry_date
+            $query = "SELECT b.first_name, b.extra2, a.lead_id, a.first_name, a.extra1, a.extra2, a.middle_initial, a.postal_code, a.city, a.phone_number, a.alt_phone, a.address1, a.entry_date
                 FROM `vicidial_list` a 
                 inner join `vicidial_list` b on a.extra7=b.lead_id 
                 left join sips_sd_reservations c on a.lead_id=c.lead_id
@@ -105,8 +104,7 @@ switch ($action) {
         $stmt = $db->prepare($query);
         $stmt->execute($variables);
         while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-           $row[9]=  date("d-m-Y H:i:s", strtotime($row[9]));
-            $row[9] = $row[9] . "<div class='view-button'>"
+            $row[12] = $row[12] . "<div class='view-button'>"
                     . "<button class='btn btn-mini icon-alone ver_cliente' data-lead_id='$row[2]' title='Ver Cliente'><i class='icon-edit'></i></button>"
                     . "<button class='btn btn-mini icon-alone criar_marcacao' data-lead_id='$row[2]' title='Marcar Consulta'><i class='icon-calendar'></i></button>"
                     . "<button class='btn btn-mini icon-alone recomendacoes' data-lead_id='$row[2]' title='Recomendados'><i class='icon-plus-sign'></i></button>"
