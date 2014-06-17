9e89e212185421d3ceef234e2f232e35ad76b16e
ricardo <ricardo@ricardo-Inspiron-5521>
2014-06-17 15:54:42 +0100

    Encomendas logadas

--- AM/ajax/requisition.php
+++ AM/ajax/requisition.php
@@ -3,15 +3,13 @@
 error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
 ini_set('display_errors', '1');
 
-
-
-
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 require "$root/AM/lib_php/db.php";
 require "$root/AM/lib_php/user.php";
 require "$root/AM/lib_php/products.php";
 require "$root/AM/lib_php/requisitions.php";
 require "$root/AM/lib_php/msg_alerts.php";
+
 foreach ($_POST as $key => $value) {
     ${$key} = $value;
 }
@@ -45,6 +43,7 @@ switch ($action) {
 
     case "editar_encomenda":
         echo json_encode($requisitions->edit_requisition($clientID, $cod_cliente));
+        $log->set("various", Logger::T_UPD, Logger::S_ENC, json_encode(array("obs" => "Codigo de Cliente editado", "lead_id" => "$clientID")));
         break;
 
     case "listar_produtos_por_encomenda":
@@ -54,9 +53,12 @@ switch ($action) {
     case "accept_requisition":
         $result = $requisitions->accept_requisition($id);
         if ($result) {
-            if ($message)
+            if ($message) {
                 $alert->make($result->user, "Encomenda Aprovada Obs. $message");
+            }
         }
+        $log->set($id, Logger::T_UPD, Logger::S_ENC, json_encode(array("obs" => "Requesição aceitada", "msg" => "$message")));
+        echo json_encode($result);
         break;
 
     case "decline_requisition":
@@ -64,7 +66,8 @@ switch ($action) {
         if ($result) {
             $alert->make($result->user, "Encomenda Rejeitada  Motivo: $message");
         }
-
+        $log->set($id, Logger::T_UPD, Logger::S_ENC, json_encode(array("obs" => "Requesição Rejeitada", "msg" => "$message")));
+        echo json_encode($result);
         break;
 
     case "check_month_requisitions":
