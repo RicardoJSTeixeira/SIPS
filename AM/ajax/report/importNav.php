1e5fd9ab52eb91ea85b73d936adfb157d5f9e407
ricardo <ricardo@ricardo-Inspiron-5521>
2014-06-30 11:18:25 +0100

    alertas navision

--- AM/ajax/report/importNav.php
+++ AM/ajax/report/importNav.php
@@ -17,20 +17,24 @@ $user = new UserLogin($db);
 $user->confirm_login();
 
 $docRoot = $_SERVER[DOCUMENT_ROOT];
-$uploadedfile=$file;
-$uploadedfile=  preg_replace("/[^-\.\_0-9a-zA-Z]/","_",$file);
-$ConvertedFile=$uploadedfile;
+$uploadedfile = $file;
+$uploadedfile = preg_replace("/[^-\.\_0-9a-zA-Z]/", "_", $file);
+$ConvertedFile = $uploadedfile;
 $uploadedfile = preg_replace("/\.txt$/i", '.csv', $uploadedfile);
 
 $ConvertCommand = "mv $docRoot/AM/ajax/files/$file $docRoot/AM/ajax/files/$uploadedfile";
-$a=passthru($ConvertCommand);
+$a = passthru($ConvertCommand);
 $ConvertCommand = "$docRoot/sips-admin/campaigns/extras/upload/sheet2tab.pl $docRoot/AM/ajax/files/$uploadedfile $docRoot/AM/ajax/files/$ConvertedFile";
-$b=passthru($ConvertCommand);
+$b = passthru($ConvertCommand);
 
 $file = fopen("$docRoot/AM/ajax/files/$ConvertedFile", "r");
 $headers = explode("\t", trim(fgets($file, 4096)));
 
 $stmt = $db->prepare("UPDATE sips_sd_reservations SET extra_id=:navid WHERE id_reservation=:id");
+$total = 0;
+$ok = 0;
+$notok = 0;
+$notoklist = array();
 while (!feof($file)) {
 
     $buffer = trim(fgets($file, 4096));
@@ -41,11 +45,16 @@ while (!feof($file)) {
 
         $LineCount++;
         $buffer[$index];
-
+        $total++;
         $stmt->execute(array(":navid" => $buffer[40], ":id" => $buffer[39]));
+        if ($stmt->rowCount()) {
+            $ok++;
+        } else {
+            $notok++;
+            $notoklist[]=array("line"=>$total+1,"navid" => $buffer[40], "id" => $buffer[39]);
+        }
     }
-    
 }
-echo json_encode(array("code"=>$ConvertCommand,"resultA"=>$a,"resultB"=>$b));
+echo json_encode(array("total" => $total, "ok" => $ok, "notok" => $notok, "notoklist" => $notoklist));
 unlink("$docRoot/AM/ajax/files/$uploadedfile");
 unlink("$docRoot/AM/ajax/files/$ConvertedFile");
