59682564e82d06509b314f35d3857bf06becaafe
ricardo <ricardo@ricardo-Inspiron-5521>
2014-07-31 13:36:17 +0100

    desligado debuger

--- AM/ajax/report/reports.php
+++ AM/ajax/report/reports.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
-ini_set('display_errors', '1');
+#error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
+#ini_set('display_errors', '1');
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 require "$root/AM/lib_php/db.php";
 
