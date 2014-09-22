ee4e8440707ab996232dffbb231719925b31d1a5
RicardoJSTeixeira <rteixeira@finesource.pt>
2014-09-22 17:23:30 +0100

    hack de filho para Filho(a) e outras

--- AM/ajax/report/consultas.php
+++ AM/ajax/report/consultas.php
@@ -17,24 +17,31 @@ $user = new UserLogin($db);
 $user->confirm_login();
 
 
+$tipoFamHack = array(
+    "marido/esposa" => "Marido/Esposa",
+    "filho" => "Filho(a)",
+    "familiar" => "Outro Familiar",
+    "amigo" => "Amigo(a)"
+);
+
 switch ($action) {
 
     case "populate_consults"://ALL MARCAÇOES
         $u = $user->getUser();
         $output['aaData'] = array();
-        $query = "SELECT extra2 'cod cliente',  a.extra_id as 'interaction log', a.lead_id 'sugar ref', id_reservation , a.entry_date, consulta_razao ,start_date, exame_razao, venda_razao, f.user, alias_code as 'salesperson code', extra1 'camp cod', IF(exame,'YES','NO'), feedback, terceira_pessoa
+        $query = "SELECT extra2 'cod cliente',  a.extra_id AS 'interaction log', a.lead_id 'sugar ref', id_reservation , a.entry_date, consulta_razao ,start_date, exame_razao, venda_razao, f.user, alias_code AS 'salesperson code', extra1 'camp cod', IF(exame,'YES','NO'), feedback, terceira_pessoa
                     FROM sips_sd_reservations a
                     INNER JOIN sips_sd_resources g ON a.id_resource = g.id_resource
                     LEFT JOIN vicidial_list d ON a.lead_id = d.lead_id
                     INNER JOIN spice_consulta f ON a.id_reservation=f.reserva_id
-                    WHERE f.closed=1 limit 20000";
+                    WHERE f.closed=1 LIMIT 20000";
         $stmt = $db->prepare($query);
         $stmt->execute();
         while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
             $terceira_pessoa = json_decode(array_pop($row));
-            
+
             if (count($terceira_pessoa)) {
-                $row[] = $terceira_pessoa->tipo;
+                $row[] = strtr($terceira_pessoa->tipo,$tipoFamHack);
                 $row[] = $terceira_pessoa->nome;
             } else {
                 $row[] = "";
