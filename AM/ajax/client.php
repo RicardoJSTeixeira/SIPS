25e8bd55da787399bbfce1b7ed4ab478337c5f5c
RicardoJSTeixeira <rteixeira@finesource.pt>
2015-02-23 17:20:28 +0000

    faltava umas cenas na query oh nao :-1:

--- AM/ajax/client.php
+++ AM/ajax/client.php
@@ -20,7 +20,7 @@ $js = array();
 $log = new Logger($db, $user->getUser());
 switch ($action) {
     case 'byName':
-        $query = "SELECT first_name, middle_initial, last_name, address1, address2, city 'local', phone_number, postal_code, date_of_birth, extra1 'codmkt', extra2 'refClient', comments FROM vicidial_list WHERE lead_id=:id LIMIT 1";
+        $query = "SELECT first_name, middle_initial, last_name, address1, address2, address3, city 'local', phone_number, alt_phone, postal_code, date_of_birth, extra1 'codmkt', extra2 'refClient', comments FROM vicidial_list WHERE lead_id=:id LIMIT 1";
         $stmt = $db->prepare($query);
         $stmt->execute(array(":id" => $id));
         $row = $stmt->fetch(PDO::FETCH_OBJ);
