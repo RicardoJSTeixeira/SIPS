6d17a83633400477d801e66c1add158cb613d024
RicardoJSTeixeira <rteixeira@finesource.pt>
2015-04-13 17:27:01 +0300

    problema de datas entre portugal e espanha

--- AM/ajax/calendar.php
+++ AM/ajax/calendar.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
 ini_set('display_errors', '1');
 
-#date_default_timezone_set('Europe/Lisbon');
+date_default_timezone_set('GMT');
 
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 $offset = filter_var($_POST["offset"]);
