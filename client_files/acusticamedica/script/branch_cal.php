c6a79aa0f76877eab55c04a9b6528da32ed99d20
RicardoJSTeixeira <rteixeira@finesource.eu>
2015-06-29 17:59:15 +0100

    refs de agent

--- client_files/acusticamedica/script/branch_cal.php
+++ client_files/acusticamedica/script/branch_cal.php
@@ -8,11 +8,11 @@ if(isset($_POST["user"])){
                     (SELECT  `ref_id` ,  `user` ,  `alias_code` ,  `cal_type` 
                     FROM  `sips_sd_agent_ref` a
                     INNER JOIN sips_sd_schedulers b ON a.id_calendar = b.id_scheduler
-                    WHERE cal_type =1)
+                    WHERE cal_type ='SCHEDULER')
                 UNION ALL (SELECT  `ref_id` ,  `user`,  `alias_code`,  `cal_type` 
                     FROM  `sips_sd_agent_ref` a
                     INNER JOIN sips_sd_resources b ON a.id_calendar = b.id_resource
-                    WHERE cal_type =2))a
+                    WHERE cal_type ='RESOURCE'))a
                     INNER JOIN vicidial_users b ON a.user = b.user
                 WHERE b.user='$user' and alias_code like '%/%' limit 1";
         
